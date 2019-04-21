78828f10d0153adb376825e169bd77cfba8718e8
Гоша <losetown343@e1.ru>
2019-04-21 21:03:20 +0500

    поправил форматирование

--- s.js
+++ s.js
@@ -1,4 +1,4 @@
 let Comb = require('csscomb');
 let comb = new Comb('csscombcust');
-// comb.processPath('C://Users/shop/WebstormProjects/CatEnergy/source/sass/');
-comb.processPath('C://Users/shop/WebstormProjects/CatEnergy/source/css/');
+comb.processPath('C://Users/shop/WebstormProjects/CatEnergy/source/sass/blocks/main.scss');
+// comb.processPath('C://Users/shop/WebstormProjects/CatEnergy/source/css/');
